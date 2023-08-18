2209da412978cb224f8be50714e10ef66c1011c5
Yiwen Yang <yangyiwen1999@outlook.com>
2023-08-18 22:58:30 +1000

    Added settings and additional patch to weights

--- Source/wpspawnrate/SpawnRate.cs
+++ Source/wpspawnrate/SpawnRate.cs
@@ -2,29 +2,64 @@
 using Verse;
 using HarmonyLib;
 using UnityEngine;
+using static WorldPawnSpawnRate.SpawnRateSettings;
 
 namespace WorldPawnSpawnRate
 {
-    [StaticConstructorOnStartup]
-    public static class WorldPawnSpawnRate 
+    public class SpawnRateSettings : ModSettings
     {
-        static WorldPawnSpawnRate()
-        {
-            Harmony harmony = new Harmony("rimworld.mod.iftclerk.wpspawnrate");
-            harmony.PatchAll();
+        public static float minSpawnRate = 0.8f;
 
-            Log.Message("World pawns spawn rates patched.");
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref minSpawnRate, "minSpawnRate", 0.8f);
+            base.ExposeData();
         }
     }
 
-    [HarmonyPatch]
-    public static class FixWorldPawnSpawnRate
+    public class SpawnRateMod : Mod
     {
-        [HarmonyPatch(typeof(PawnGenerator), "ChanceToRedressAnyWorldPawn")]
-        [HarmonyPostfix]
-        public static void FixSpawnRate(ref float __result)
+        // SpawnRateSettings settings;
+
+        public SpawnRateMod(ModContentPack content) : base(content)
         {
-            __result = Mathf.Max(0.8f, __result);
+            base.GetSettings<SpawnRateSettings>();
+
+            // Patch with Harmony
+            Harmony harmony = new Harmony("iftclerk.wpspawnrate");
+            harmony.Patch(AccessTools.Method(typeof(Verse.PawnGenerator), "ChanceToRedressAnyWorldPawn"),
+                postfix: new HarmonyMethod(typeof(SpawnRateMod), nameof(PatchSpawnRate)));
+            harmony.Patch(AccessTools.Method(typeof(Verse.PawnGenerator), "WorldPawnSelectionWeight"),
+                postfix: new HarmonyMethod(typeof(SpawnRateMod), nameof(PatchWeight)));
+
+            Log.Message("World pawns spawn rates patched.");
+        }
+
+        // The patches
+        public static void PatchSpawnRate(ref float __result)
+        {
+            __result = Mathf.Max(minSpawnRate, __result);
+        }
+        public static void PatchWeight(ref float __result)
+        {
+            __result = 1.0f;
+        }
+
+        // Settings GUI
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Minimum spawn rate for world pawns: " + Math.Round(minSpawnRate, 2));
+            minSpawnRate = listingStandard.Slider(minSpawnRate, 0.0f, 1.0f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        // Show in settings menu
+        public override string SettingsCategory()
+        {
+            return "WorldPawnSpawnRate";
         }
     }
 }
