1425ab69337b4c857fbcfe0894a98a0bf5ba64c0
IFTClerk <54573631+IFTClerk@users.noreply.github.com>
2023-08-18 19:27:47 +1000

    Initial commit

---
+++ Source/wpspawnrate/SpawnRate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+
+namespace WorldPawnSpawnRate
+{
+    [StaticConstructorOnStartup]
+    public static class WorldPawnSpawnRate 
+    {
+        static WorldPawnSpawnRate()
+        {
+            Harmony harmony = new Harmony("rimworld.mod.iftclerk.wpspawnrate");
+            harmony.PatchAll();
+
+            Log.Message("World pawns spawn rates patched.");
+        }
+    }
+
+    [HarmonyPatch]
+    public static class FixWorldPawnSpawnRate
+    {
+        [HarmonyPatch(typeof(PawnGenerator), "ChanceToRedressAnyWorldPawn")]
+        [HarmonyPostfix]
+        public static void FixSpawnRate(ref float __result)
+        {
+            __result = Mathf.Max(0.8f, __result);
+        }
+    }
+}
