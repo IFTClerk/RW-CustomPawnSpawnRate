9384390b83bb0afd04bcc8942e578e0d115b0ee8
IFTClerk <54573631+IFTClerk@users.noreply.github.com>
2023-08-20 06:28:54 +1000

    Overhaul approach for mod

---
+++ Source/customspawnrate/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+
+namespace CustomPawnSpawnRate
+{
+    public class SpawnRateSettings : ModSettings
+    {
+        public static float minSpawnRate = 0.0f;
+        public static float probSpawnRate = 0.5f;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref minSpawnRate, "minSpawnRate", 0.0f);
+            Scribe_Values.Look(ref probSpawnRate, "probSpawnRate", 0.5f);
+            base.ExposeData();
+        }
+    }
+
+}
