af4e0becd27984e246f12924bce5c9c27a992823
Yiwen Yang <yangyiwen1999@outlook.com>
2023-08-20 06:28:54 +1000

    Overhaul approach for mod

---
+++ Source/customspawnrate/Mod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+using static CustomPawnSpawnRate.SpawnRateSettings;
+
+namespace CustomPawnSpawnRate
+{
+    public class SpawnRateMod : Mod
+    {
+        public SpawnRateMod(ModContentPack content) : base(content)
+        {
+            // Load settings from file
+            base.GetSettings<SpawnRateSettings>();
+
+            // Patch with Harmony
+            Harmony harmony = new Harmony("iftclerk.wpspawnrate");
+            harmony.Patch(AccessTools.Method(typeof(Verse.PawnGenerator), "ChanceToRedressAnyWorldPawn"),
+                postfix: new HarmonyMethod(typeof(SpawnRatePatches), nameof(SpawnRatePatches.PatchSpawnRate)));
+            harmony.Patch(AccessTools.Method(typeof(RimWorld.Planet.WorldPawnGC), "GetCriticalPawnReason"),
+                postfix: new HarmonyMethod(typeof(SpawnRatePatches), nameof(SpawnRatePatches.PatchWorldPawnGC)));
+            harmony.Patch(AccessTools.Method(typeof(Verse.PawnGenerator), "GenerateOrRedressPawnInternal"),
+                prefix: new HarmonyMethod(typeof(SpawnRatePatches), nameof(SpawnRatePatches.PatchPawnGenerator)));
+
+            Log.Message("World pawns spawn rates patched.");
+        }
+
+        // Settings GUI
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Minimum spawn rate for custom pawns (old method, keep at 0 to disable): " + Math.Round(minSpawnRate, 2));
+            minSpawnRate = listingStandard.Slider(minSpawnRate, 0.0f, 1.0f);
+            listingStandard.Label("Forced spawn rate for custom pawns: " + Math.Round(probSpawnRate, 2));
+            probSpawnRate = listingStandard.Slider(probSpawnRate, 0.0f, 1.0f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        // Show in settings menu
+        public override string SettingsCategory()
+        {
+            return "CustomPawnSpawnRate";
+        }
+    }
+}
---
+++ Source/customspawnrate/Patches.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using RimWorld.Planet;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using System.Reflection;
+
+namespace CustomPawnSpawnRate
+{
+    public static class SpawnRatePatches
+    {
+        static MethodInfo validPawn = AccessTools.Method(typeof(Verse.PawnGenerator), "IsValidCandidateToRedress");
+        
+        private static bool _IsCustomPawn(Pawn pawn)
+        {
+            if (pawn.Ideo == Find.FactionManager.OfPlayer.ideos.PrimaryIdeo)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // The patches
+        public static void PatchSpawnRate(ref float __result)
+        {
+            __result = Mathf.Max(SpawnRateSettings.minSpawnRate, __result);
+        }
+        public static void PatchWorldPawnGC(ref string __result, Pawn pawn)
+        {
+            if (_IsCustomPawn(pawn))
+            {
+                __result = "CustomPawn";
+            }
+        }
+        public static bool PatchPawnGenerator(ref Pawn __result, PawnGenerationRequest request)
+        {
+            if (request.AllowedDevelopmentalStages.Newborn() || request.ForceGenerateNewPawn)
+            {
+                // Run original method if game wants new pawn
+                return true;
+            }
+
+            IEnumerable<Pawn> customPawns = Find.WorldPawns.GetPawnsBySituation(WorldPawnSituation.Free).Where((Pawn p) => _IsCustomPawn(p));
+            bool oldParam = request.WorldPawnFactionDoesntMatter;
+            request.WorldPawnFactionDoesntMatter = true;
+            IEnumerable<Pawn> freePawns = customPawns.Where((Pawn p) => (bool)validPawn.Invoke(null, new object[] { p, request }));
+            if (Rand.Chance(SpawnRateSettings.probSpawnRate) && freePawns.TryRandomElementByWeight((Pawn x) => 1.0f, out Pawn pawn))
+            {
+                // Debug
+                //Log.Message("[WP]Got world pawn, redressing.");
+
+                Verse.PawnGenerator.RedressPawn(pawn, request);
+                Find.WorldPawns.RemovePawn(pawn);
+
+                pawn.Ideo?.Notify_MemberGenerated(pawn, request.AllowedDevelopmentalStages.Newborn());
+                Find.Scenario?.Notify_PawnGenerated(pawn, request.Context, true);
+
+                __result = pawn;
+                request.WorldPawnFactionDoesntMatter = oldParam;
+                return false;
+            }
+            // Debug
+            //Log.Message("[WP]Did not get pawn, proceeding to original method.");
+            request.WorldPawnFactionDoesntMatter = oldParam;
+            return true;
+        }
+
+
+    }
+}
