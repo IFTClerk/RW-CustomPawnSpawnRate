49256a239d6672e2f40e9378ac958268f968c7c5
Yiwen Yang <yangyiwen1999@outlook.com>
2023-08-20 10:24:59 +1000

    Prepare gitignore

--- Source/customspawnrate/Settings.cs
+++ Source/customspawnrate/Settings.cs
@@ -5,8 +5,8 @@ namespace CustomPawnSpawnRate
 {
     public class SpawnRateSettings : ModSettings
     {
-        public static float minSpawnRate = 0.0f;
-        public static float probSpawnRate = 0.5f;
+        public static float minSpawnRate = 0.0f; // Minimum spawn rate of world pawns (obsolete)
+        public static float probSpawnRate = 0.5f; // Spawn rate of custom pawns
 
         public override void ExposeData()
         {
